20e39ffb84c7920f35342a50a00b175fbef2ff68
Alexandre Magno <alexanmtz@gmail.com>
2016-02-20 22:21:10 -0200

    setting up navigation when needed

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,11 +4,24 @@ $(function(){
 	$('#carousel-intro').carousel({interval: false});
 	
  	$('#carousel-intro').on('swiperight', function(e){
- 		console.info(e.target);
 		$('#carousel-intro').carousel('prev');
  	}).on('swipeleft', function(){
 		$('#carousel-intro').carousel('next');
  	});
+ 	
+ 	$('.carousel-right').on('click',function(){
+ 		$('#carousel-intro').carousel('next');
+ 		return false;
+ 	});
+ 	
+ 	$('.carousel-left').on('click',function(){
+ 		$('#carousel-intro').carousel('prev');
+ 		return false;
+ 	});
+ 	
+ 	if(window.DocumentTouch && document instanceof DocumentTouch) {
+ 		$('.carousel-navigation').hide();
+ 	}
 	
 	
 });
