4071f08f8a83b7d9c5c6d9c0bd6a0fb79042e67a
Alexandre Magno <alexanmtz@gmail.com>
2016-03-06 14:25:28 -0300

    user profile, login and signup basic

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,19 +2,19 @@ class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+   def new
+     super
+   end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+   def create
+     super
+   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+   def destroy
+     super
+   end
 
   # protected
 
