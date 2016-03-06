e3ca5b20c352e74bfed3249f5b1527d07e3273fc
Alexandre Magno <alexanmtz@gmail.com>
2016-03-06 13:10:26 -0300

    a first draw of a login interface

--- test/controllers/tours_controller_test.rb
+++ test/controllers/tours_controller_test.rb
@@ -1,44 +1,44 @@
-# require 'test_helper'
-# 
-# class ToursControllerTest < ActionController::TestCase
-  # setup do
-    # @tour = tours(:morro)
-  # end
-# 
-  # test "should get index" do
-    # get :index
-    # assert_response :success
-    # assert_not_nil assigns(:tours)
-  # end
-# 
-  # test "should get new" do
-    # get :new
-    # assert_response :success
-  # end
-# 
+require 'test_helper'
+
+class ToursControllerTest < ActionController::TestCase
+  setup do
+    @tour = tours(:morro)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:tours)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
   # test "should create tour" do
     # assert_difference('Tour.count') do
-      # post :create, tour: {  }
+      # post :create, tour: {}
     # end
 # 
     # assert_redirected_to tour_path(assigns(:tour))
   # end
-# 
-  # test "should show tour" do
-    # get :show, id: @tour
-    # assert_response :success
-  # end
-# 
-  # test "should get edit" do
-    # get :edit, id: @tour
-    # assert_response :success
-  # end
-# 
-  # test "should update tour" do
-    # patch :update, id: @tour, tour: {  }
-    # assert_redirected_to tour_path(assigns(:tour))
-  # end
-# 
+
+  test "should show tour" do
+    get :show, id: @tour
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @tour
+    assert_response :success
+  end
+
+  test "should update tour" do
+    patch :update, id: @tour, tour: {  }
+    assert_redirected_to tour_path(assigns(:tour))
+  end
+
   # test "should destroy tour" do
     # assert_difference('Tour.count', -1) do
       # delete :destroy, id: @tour
@@ -46,4 +46,4 @@
 # 
     # assert_redirected_to tours_path
   # end
-# end
+end
