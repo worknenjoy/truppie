884cb0863ed9a59a96e213259431f18dbb0916a5
Alexandre Magno <alexanmtz@gmail.com>
2016-03-23 00:50:18 -0300

    setting heroku mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   
   config.action_mailer.smtp_settings = {
