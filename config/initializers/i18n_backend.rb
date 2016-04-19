d22007da11814c166ed5d6b1f2eb9386fb8230ff
Alexandre Magno <alexanmtz@gmail.com>
2016-04-19 20:04:22 -0300

    commenting i18n new feature

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,2 +1,2 @@
-TRANSLATION_STORE = Redis.new
-I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new(TRANSLATION_STORE), I18n.backend)
+#TRANSLATION_STORE = Redis.new
+#I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new(TRANSLATION_STORE), I18n.backend)
