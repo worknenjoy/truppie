bf1c72dd0bcbaac33ab287d6408e060fd94d3285
Alexandre Magno <alexanmtz@gmail.com>
2016-05-02 23:16:35 -0300

    deleting paperclip file

--- config/initializers/paperclip.rb
+++
@@ -1,4 +0,0 @@
-# config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
