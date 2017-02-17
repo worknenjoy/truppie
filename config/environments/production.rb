e6239887353379c8e740e4a5eea871b7b2b70229
Alexandre Magno <alexanmtz@gmail.com>
2017-02-17 17:15:57 +0100

    https for papeclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_region => ENV['AWS_REGION'],
     :s3_host_name => 's3-sa-east-1.amazonaws.com',
+    :s3_protocol => :https,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
