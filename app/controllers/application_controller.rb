beaf6c92fe0b41f56502eacf37679396868bed1b
Alexandre Magno <alexanmtz@gmail.com>
2017-02-18 11:32:19 +0100

    no ssl again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
-  force_ssl if: :ssl_configured?
+  #force_ssl if: :ssl_configured?
 
-  def ssl_configured?
-    !Rails.env.development?
-  end  
+  #def ssl_configured?
+  #  !Rails.env.development?
+  #end  
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  #before_filter :expire_hsts
+  before_filter :expire_hsts
   
-  #private
-  #def expire_hsts
-  #  response.headers["Strict-Transport-Security"] = 'max-age=0'
-  #end
+  private
+  def expire_hsts
+    response.headers["Strict-Transport-Security"] = 'max-age=0'
+  end
   
 end
