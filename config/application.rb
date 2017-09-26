f656bb8dbbcf8b0a4b5feedae05eecfcb34ef625
Alexandre Magno <alexanmtz@gmail.com>
2017-09-26 12:26:16 +0200

    config.assets.initialize_on_precompile set to try to fix heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Truppie
     
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
+    config.assets.initialize_on_precompile = false
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
