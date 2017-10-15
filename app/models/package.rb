d901a4316530b91773f516a8da490fafda7bbb29
Alexandre Magno <alexanmtz@gmail.com>
2017-10-15 19:40:29 +0200

    packages setup with descriptions

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,17 +1,19 @@
 class Package < ActiveRecord::Base
   
   has_and_belongs_to_many :tours
+  has_and_belongs_to_many :guidebooks
   
   def price
-    case self.tours.first.currency
+    case self.try(:currency)
     when 'BRL'
       "<small>R$</small> " + self.value.to_s
     when 'US'
       "<small>$</small> " + self.value.to_s
     when 'EURO'
       "<small>€</small> " + self.value.to_s
-    else
-      self.value
+      else
+      # get the right currency
+      "<small>R$</small> " + self.value.to_s
     end
   end
   
