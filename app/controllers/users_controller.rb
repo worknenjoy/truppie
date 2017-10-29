6f39c6cb183bceb0e5501d53e2f10b2fce8514c1
Leonardo <leo.spaula@gmail.com>
2017-10-29 19:46:55 -0200

    Add action and view to follow organizer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+	def follow
+    @organizer = Organizer.find(params[:organizer_id])
+    current_user.follow @organizer
+    OrganizerMailer.new_follower(@organizer).deliver_now
+  end
+  
+  def unfollow
+    @organizer = Organizer.find(params[:organizer_id])
+    current_user.unfollow @organizer
+  end
 end
