34fb0f476e560daa30873c41b2c3e819574cf251
Leonardo <leo.spaula@gmail.com>
2017-10-31 11:02:12 -0200

    Redirect and Follow after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -104,7 +104,8 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => {
       :registrations => "users/registrations",
-      :omniauth_callbacks => "users/omniauth_callbacks"
+      :omniauth_callbacks => "users/omniauth_callbacks",
+      :sessions => "users/sessions"
   }
 
   resources :users do
