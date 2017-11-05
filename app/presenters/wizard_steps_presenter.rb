caa55068e8e4c440e736272032801ac512df85a5
Hugo Hernani <hugo@quezmedia.com>
2017-11-04 23:27:40 -0300

    Get rid of class variables and improve step verification to avoid too much database check

--- app/presenters/wizard_steps_presenter.rb
+++ app/presenters/wizard_steps_presenter.rb
@@ -1,30 +1,29 @@
 class WizardStepsPresenter < BasePresenter
-  @@wizard_step = 1
-  @@activated = @@skip_active = false
-  def initialize(step, name, link, done)
-    @step = step
-    @name = name
-    @link = link
-    @done = done
+  def initialize(profile_verified, account_verified, bank_account_verified, any_tour_created_verified)
+    @next_step = 1
+    @verified_steps = {
+      1 => !!profile_verified,
+      2 => !!account_verified,
+      3 => !!bank_account_verified,
+      4 => !!any_tour_created_verified
+    }
   end
 
-  def self.steps(steps_limit, organizer)
-    @@wizard_step = 0
-    completely_verified = organizer.verified? && personal_account_verified(organizer) &&
-      organizer.marketplace.try(:bank_account_verified) && organizer.tours.any?
-    completely_verified ? '' : yield(self)
-  end
-
-  def self.render(step_name, step_link, done = false)
-    @@skip_active = @@activated
-    @@activated = true if !done && !@@skip_active
-    new(@@wizard_step+=1, step_name, step_link, done).render_step
-  end
-
-  def render_step
-    h.link_to @link, class: "step #{active_css} #{done_css}", data: { desc: @name } do
-      h.concat(!@@activated ? h.content_tag(:i, '', class: 'fa fa-check') : @step)
+  def render(current_step, step_name, step_link)
+    h.link_to step_link, class: "step #{active_css(current_step)} #{done_css(current_step)}", data: { desc: step_name } do
+      h.concat(done?(current_step)? h.content_tag(:i, '', class: 'fa fa-check') : current_step)
     end
+  end
+
+  def completely_verified?
+    @verified_steps.all?{|step, verified| verified }
+  end
+
+  # Wizard instance generator
+  def self.steps(organizer)
+    wizard_instance = new(organizer.verified?, personal_account_verified(organizer),
+      organizer.marketplace.try(:bank_account_verified), organizer.tours.any?)
+    wizard_instance.completely_verified?? '' : yield(wizard_instance)
   end
 
   private
@@ -34,11 +33,20 @@ class WizardStepsPresenter < BasePresenter
       organizer.marketplace.account_user_data_verified
   end
 
-  def active_css
-    return @@activated && !@@skip_active ? 'active' : ''
+  def active_css(current_step)
+    @verified_steps[current_step-1] && !@verified_steps[current_step] ? 'active' : ''
   end
 
-  def done_css
-    @done ? 'done' : ''
+  def done_css(current_step)
+    if done?(current_step)
+      @next_step = current_step + 1
+      return 'done'
+    else
+      ''
+    end
+  end
+
+  def done?(current_step)
+    @verified_steps.select{|step, verified| step <= current_step }.all?{|step, verified| verified }
   end
 end
