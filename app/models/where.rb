8f577d2b90221f7bd54b59657da6f53b8e454804
Alexandre Magno <alz@tradeshift.com>
2017-12-04 23:28:25 -0200

    structure a basic from places

--- app/models/where.rb
+++ app/models/where.rb
@@ -14,6 +14,21 @@ class Where < ActiveRecord::Base
   before_save :set_time_zone
 
 
+  def picture
+    if self.try(:place_id) and !self.place_id.blank?
+      @client = GooglePlaces::Client.new('AIzaSyBd61mfgh_26mtP1GFqaakPAHaaNI84j-A')
+      @placeid = self.place_id
+      @place = @client.spot(@placeid)
+      picture_ref = @place.photos[0].photo_reference
+      return "https://maps.googleapis.com/maps/api/place/photo?sensor=false&maxwidth=3000&maxheight=3000&key=AIzaSyBd61mfgh_26mtP1GFqaakPAHaaNI84j-A&photoreference=#{picture_ref}"
+    end
+    if self.backgrounds.any?
+      return b.first.picture.url(:medium)
+    end
+    return 'http://lorempixel.com/400/200/'
+  end
+
+
   def to_param
     "#{id} #{name}".parameterize
   end
