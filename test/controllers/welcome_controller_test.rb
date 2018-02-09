4ec3ed7435f4bb06e951e6b64e2f0c4ecca34b9d
Danilo Cândido <danilod07@gmail.com>
2018-02-09 01:39:31 -0300

    start testing organizer

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,12 +1,16 @@
-include Devise::TestHelpers
+
 require 'test_helper'
+require 'json'
 
 class WelcomeControllerTest < ActionController::TestCase
-
-
+  include Devise::TestHelpers
+  
   test "the truth" do
-    skip("setup auth")
-     get :organizer
-     assert_response :success
+    sign_in users(:alexandre)
+  end
+
+  test 'when a guide is online go to profile guide' do
+    get :organizer
+    assert_response :success
   end
 end
