893a39e0464ce6f18197b790b7079b6c2e8778e9
Danilo Cândido <danilod07@gmail.com>
2018-02-12 10:53:47 -0300

    creating organize in test to redirect to profile page

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -5,12 +5,26 @@ require 'json'
 class WelcomeControllerTest < ActionController::TestCase
   include Devise::TestHelpers
   
-  test "the truth" do
+  setup do
     sign_in users(:alexandre)
+
+    set_organizer = {
+       name: "Utópicos mundo afora",
+       description: "uma agencia utopica",
+       email: "utopicos@gmail.com",
+       website: "http://website",
+       facebook: "a facebook",
+       twitter: "a twitter",
+       instagram: "a instagram",
+       phone: "a phone",
+       user_id: users(:alexandre).id
+     }
+
+     @organizer = Organizer.create!(set_organizer)
   end
 
-  test 'when a guide is online go to profile guide' do
+  test 'when a guide is online redirect to profile guide page' do
     get :organizer
-    assert_response :success
+    assert_redirected_to profile_edit_organizer_path(@organizer)
   end
 end
