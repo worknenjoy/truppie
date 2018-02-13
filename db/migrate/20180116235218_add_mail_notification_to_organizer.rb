97ac7d2f81e2da5d00335cf4be479234623f4827
Alexandre Magno <alexanmtz@gmail.com>
2018-02-13 10:57:13 +0100

    set notification by default

--- db/migrate/20180116235218_add_mail_notification_to_organizer.rb
+++ db/migrate/20180116235218_add_mail_notification_to_organizer.rb
@@ -1,5 +1,5 @@
 class AddMailNotificationToOrganizer < ActiveRecord::Migration
   def change
-    add_column :organizers, :mail_notification, :boolean
+    add_column :organizers, :mail_notification, :boolean, default: true
   end
 end
