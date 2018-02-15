bb0caede52f8e377b752947ebde57a9bbba78aee
Danilo Cândido <danilod07@gmail.com>
2018-02-15 18:23:22 -0300

    redirect logged user to profile page (#70)
    * if organize is created redirect to profile_edit_organizer_path
    
    * start testing organizer
    
    * creating organize in test to redirect to profile page
    
    * when organizer is not found go to new organizer
    
    * check if is an object without any?
    
    * back to creaing @new_organizer
    
    * when a guide is not admin displays new organizer form

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,12 +1,34 @@
-include Devise::TestHelpers
+
 require 'test_helper'
+require 'json'
 
 class WelcomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+  
+  setup do
+    sign_in users(:alexandre)
 
+    @set_organizer = {
+       name: "Utópicos mundo afora",
+       description: "uma agencia utopica",
+       email: "utopicos@gmail.com",
+       website: "http://website",
+       facebook: "a facebook",
+       twitter: "a twitter",
+       instagram: "a instagram",
+       phone: "a phone",
+       user_id: users(:alexandre).id
+     }
+  end
+    
+  test 'when a guide is online redirect to profile guide page' do
+    @organizer = Organizer.create!(@set_organizer)
+    get :organizer
+    assert_redirected_to profile_edit_organizer_path(@organizer)
+  end
 
-  test "the truth" do
-    skip("setup auth")
-     get :organizer
-     assert_response :success
+  test 'when a guide is not admin displays new organizer form' do
+    get :organizer
+    assert_response :success
   end
 end
