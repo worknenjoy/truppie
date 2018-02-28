53a828e055962726bc348168f3fa95e6fcd21bc3
lucianojfj <lucianojfj@gmail.com>
2018-02-28 03:59:01 -0300

    Refactoring 'mountain-form.js' #77

--- app/assets/javascripts/mountain-form.js
+++ app/assets/javascripts/mountain-form.js
@@ -8,114 +8,59 @@ window.addEventListener("load", function () {
     $(document).on("click", ".container-mountain #prev3", prevStep3);
     $(document).on("click", ".container-mountain #prev4", prevStep4);
 
-    // ======== SECTION 1 =============== //
-
     function nextStep(e) {
-        document.getElementById("first").style.display = "none";
-        document.getElementById("second").style.display = "inline";
-
-        document.getElementById("dot1").style.opacity = "0.3";
-        document.getElementById("dot2").style.opacity = "1";
-        document.getElementById("dot3").style.opacity = "0.3";
-        document.getElementById("dot4").style.opacity = "0.3";
-        document.getElementById("dot5").style.opacity = "0.3";
-        e.preventDefault();
-
+      updateTab("second", "dot2", e);
     }
 
-    // ======== SECTION 2 =============== //
-
     function prevStep(e) {
-        document.getElementById("second").style.display = "none";
-        document.getElementById("first").style.display = "inline";
-
-        document.getElementById("dot1").style.opacity = "1";
-        document.getElementById("dot2").style.opacity = "0.3";
-        document.getElementById("dot3").style.opacity = "0.3";
-        document.getElementById("dot4").style.opacity = "0.3";
-        document.getElementById("dot5").style.opacity = "0.3";
-        e.preventDefault();
+      updateTab("first", "dot1", e);
     }
 
     function nextStep2(e) {
-        document.getElementById("second").style.display = "none";
-        document.getElementById("third").style.display = "inline";
-
-        document.getElementById("dot1").style.opacity = "0.3";
-        document.getElementById("dot2").style.opacity = "0.3";
-        document.getElementById("dot3").style.opacity = "1";
-        document.getElementById("dot4").style.opacity = "0.3";
-        document.getElementById("dot5").style.opacity = "0.3";
-        e.preventDefault();
-
+      updateTab("third", "dot3", e);
     }
 
-    // ======== SECTION 3 =============== //
-
     function prevStep2(e) {
-        document.getElementById("third").style.display = "none";
-        document.getElementById("second").style.display = "inline";
-
-        document.getElementById("dot1").style.opacity = "0.3";
-        document.getElementById("dot2").style.opacity = "1";
-        document.getElementById("dot3").style.opacity = "0.3";
-        document.getElementById("dot4").style.opacity = "0.3";
-        document.getElementById("dot5").style.opacity = "0.3";
-        e.preventDefault();
+      updateTab("second", "dot2", e);
     }
 
     function nextStep3(e) {
-        document.getElementById("third").style.display = "none";
-        document.getElementById("fourth").style.display = "inline";
-
-        document.getElementById("dot1").style.opacity = "0.3";
-        document.getElementById("dot2").style.opacity = "0.3";
-        document.getElementById("dot3").style.opacity = "0.3";
-        document.getElementById("dot4").style.opacity = "1";
-        document.getElementById("dot5").style.opacity = "0.3";
-        e.preventDefault();
-
+      updateTab("fourth", "dot4", e);
     }
 
-    // ======== SECTION 4 =============== //
-
     function prevStep3(e) {
-        document.getElementById("fourth").style.display = "none";
-        document.getElementById("third").style.display = "inline";
-
-        document.getElementById("dot1").style.opacity = "0.3";
-        document.getElementById("dot2").style.opacity = "0.3";
-        document.getElementById("dot3").style.opacity = "1";
-        document.getElementById("dot4").style.opacity = "0.3";
-        document.getElementById("dot5").style.opacity = "0.3";
-        e.preventDefault();
+      updateTab("third", "dot3", e);
     }
 
     function nextStep4(e) {
-        document.getElementById("fourth").style.display = "none";
-        document.getElementById("fifth").style.display = "inline";
-
-        document.getElementById("dot1").style.opacity = "0.3";
-        document.getElementById("dot2").style.opacity = "0.3";
-        document.getElementById("dot3").style.opacity = "0.3";
-        document.getElementById("dot4").style.opacity = "0.3";
-        document.getElementById("dot5").style.opacity = "1";
-        e.preventDefault();
-
+      updateTab("fifth", "dot5", e);
     }
-
-    // ======== SECTION 5 =============== //
 
     function prevStep4(e) {
-        document.getElementById("fifth").style.display = "none";
-        document.getElementById("fourth").style.display = "inline";
-
-        document.getElementById("dot1").style.opacity = "0.3";
-        document.getElementById("dot2").style.opacity = "0.3";
-        document.getElementById("dot3").style.opacity = "0.3";
-        document.getElementById("dot4").style.opacity = "1";
-        document.getElementById("dot5").style.opacity = "0.3";
-        e.preventDefault();
+      updateTab("fourth", "dot4", e);
     }
 
+    function updateTab(sectionId, dotId, e) {
+      goToSection(sectionId);
+      setCurrentDotActive(dotId);
+      e.preventDefault();
+    }
+
+    function goToSection(sectionId) {
+      document.getElementById("first").style.display = "none";
+      document.getElementById("second").style.display = "none";
+      document.getElementById("third").style.display = "none";
+      document.getElementById("fourth").style.display = "none";
+      document.getElementById("fifth").style.display = "none";
+      document.getElementById(sectionId).style.display = "inline";
+    }
+
+    function setCurrentDotActive(dotId) {
+      document.getElementById("dot1").style.opacity = "0.3";
+      document.getElementById("dot2").style.opacity = "0.3";
+      document.getElementById("dot3").style.opacity = "0.3";
+      document.getElementById("dot4").style.opacity = "0.3";
+      document.getElementById("dot5").style.opacity = "0.3";
+      document.getElementById(dotId).style.opacity = "1";
+    }
 });
