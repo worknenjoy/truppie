02eec5cad3d8c201d3ac377d773efa2fe76601e6
lucianojfj <lucianojfj@users.noreply.github.com>
2018-03-04 10:53:32 -0300

    '/welcome/organizer' page not working properly (#78)
    * Fix #77
    
    * Mitigate id collision when searching for html element #77
    
    * Refactoring 'mountain-form.js' #77

--- app/assets/javascripts/mountain-form.js
+++ app/assets/javascripts/mountain-form.js
@@ -1,145 +1,66 @@
 window.addEventListener("load", function () {
-    if(document.getElementById("dot1")) {
-        document.getElementById("dot1").style.opacity = "1";
-        document.getElementById("dot2").style.opacity = "0.3";
-        document.getElementById("dot3").style.opacity = "0.3";
-        document.getElementById("dot4").style.opacity = "0.3";
-        document.getElementById("dot5").style.opacity = "0.3";
+    $(document).on("click", ".container-mountain #continue", nextStep);
+    $(document).on("click", ".container-mountain #continue2", nextStep2);
+    $(document).on("click", ".container-mountain #continue3", nextStep3);
+    $(document).on("click", ".container-mountain #continue4", nextStep4);
+    $(document).on("click", ".container-mountain #prev", prevStep);
+    $(document).on("click", ".container-mountain #prev2", prevStep2);
+    $(document).on("click", ".container-mountain #prev3", prevStep3);
+    $(document).on("click", ".container-mountain #prev4", prevStep4);
 
-        var next = document.getElementById("continue");
-        next.addEventListener("click", nextStep);
-
-        var next2 = document.getElementById("continue2");
-        next2.addEventListener("click", nextStep2);
-
-        var next3 = document.getElementById("continue3");
-        next3.addEventListener("click", nextStep3);
-
-        var next4 = document.getElementById("continue4");
-        next4.addEventListener("click", nextStep4);
-
-        var prev = document.getElementById("prev");
-        prev.addEventListener("click", prevStep);
-
-        var prev2 = document.getElementById("prev2");
-        prev2.addEventListener("click", prevStep2);
-
-        var prev3 = document.getElementById("prev3");
-        prev3.addEventListener("click", prevStep3);
-
-        var prev4 = document.getElementById("prev4");
-        prev4.addEventListener("click", prevStep4);
-
-        // ======== SECTION 1 =============== //
-
-        function nextStep(e) {
-            document.getElementById("first").style.display = "none";
-            document.getElementById("second").style.display = "inline";
-
-            document.getElementById("dot1").style.opacity = "0.3";
-            document.getElementById("dot2").style.opacity = "1";
-            document.getElementById("dot3").style.opacity = "0.3";
-            document.getElementById("dot4").style.opacity = "0.3";
-            document.getElementById("dot5").style.opacity = "0.3";
-            e.preventDefault();
-
-        }
-
-        // ======== SECTION 2 =============== //
-
-        function prevStep(e) {
-            document.getElementById("second").style.display = "none";
-            document.getElementById("first").style.display = "inline";
-
-            document.getElementById("dot1").style.opacity = "1";
-            document.getElementById("dot2").style.opacity = "0.3";
-            document.getElementById("dot3").style.opacity = "0.3";
-            document.getElementById("dot4").style.opacity = "0.3";
-            document.getElementById("dot5").style.opacity = "0.3";
-            e.preventDefault();
-        }
-
-        function nextStep2(e) {
-            document.getElementById("second").style.display = "none";
-            document.getElementById("third").style.display = "inline";
-
-            document.getElementById("dot1").style.opacity = "0.3";
-            document.getElementById("dot2").style.opacity = "0.3";
-            document.getElementById("dot3").style.opacity = "1";
-            document.getElementById("dot4").style.opacity = "0.3";
-            document.getElementById("dot5").style.opacity = "0.3";
-            e.preventDefault();
-
-        }
-
-
-        // ======== SECTION 3 =============== //
-
-        function prevStep2(e) {
-            document.getElementById("third").style.display = "none";
-            document.getElementById("second").style.display = "inline";
-
-            document.getElementById("dot1").style.opacity = "0.3";
-            document.getElementById("dot2").style.opacity = "1";
-            document.getElementById("dot3").style.opacity = "0.3";
-            document.getElementById("dot4").style.opacity = "0.3";
-            document.getElementById("dot5").style.opacity = "0.3";
-            e.preventDefault();
-        }
-
-        function nextStep3(e) {
-            document.getElementById("third").style.display = "none";
-            document.getElementById("fourth").style.display = "inline";
-
-            document.getElementById("dot1").style.opacity = "0.3";
-            document.getElementById("dot2").style.opacity = "0.3";
-            document.getElementById("dot3").style.opacity = "0.3";
-            document.getElementById("dot4").style.opacity = "1";
-            document.getElementById("dot5").style.opacity = "0.3";
-            e.preventDefault();
-
-        }
-
-        // ======== SECTION 4 =============== //
-
-        function prevStep3(e) {
-            document.getElementById("fourth").style.display = "none";
-            document.getElementById("third").style.display = "inline";
-
-            document.getElementById("dot1").style.opacity = "0.3";
-            document.getElementById("dot2").style.opacity = "0.3";
-            document.getElementById("dot3").style.opacity = "1";
-            document.getElementById("dot4").style.opacity = "0.3";
-            document.getElementById("dot5").style.opacity = "0.3";
-            e.preventDefault();
-        }
-
-        function nextStep4(e) {
-            document.getElementById("fourth").style.display = "none";
-            document.getElementById("fifth").style.display = "inline";
-
-            document.getElementById("dot1").style.opacity = "0.3";
-            document.getElementById("dot2").style.opacity = "0.3";
-            document.getElementById("dot3").style.opacity = "0.3";
-            document.getElementById("dot4").style.opacity = "0.3";
-            document.getElementById("dot5").style.opacity = "1";
-            e.preventDefault();
-
-        }
-
-        // ======== SECTION 5 =============== //
-
-        function prevStep4(e) {
-            document.getElementById("fifth").style.display = "none";
-            document.getElementById("fourth").style.display = "inline";
-
-            document.getElementById("dot1").style.opacity = "0.3";
-            document.getElementById("dot2").style.opacity = "0.3";
-            document.getElementById("dot3").style.opacity = "0.3";
-            document.getElementById("dot4").style.opacity = "1";
-            document.getElementById("dot5").style.opacity = "0.3";
-            e.preventDefault();
-        }
+    function nextStep(e) {
+      updateTab("second", "dot2", e);
     }
 
+    function prevStep(e) {
+      updateTab("first", "dot1", e);
+    }
+
+    function nextStep2(e) {
+      updateTab("third", "dot3", e);
+    }
+
+    function prevStep2(e) {
+      updateTab("second", "dot2", e);
+    }
+
+    function nextStep3(e) {
+      updateTab("fourth", "dot4", e);
+    }
+
+    function prevStep3(e) {
+      updateTab("third", "dot3", e);
+    }
+
+    function nextStep4(e) {
+      updateTab("fifth", "dot5", e);
+    }
+
+    function prevStep4(e) {
+      updateTab("fourth", "dot4", e);
+    }
+
+    function updateTab(sectionId, dotId, e) {
+      goToSection(sectionId);
+      setCurrentDotActive(dotId);
+      e.preventDefault();
+    }
+
+    function goToSection(sectionId) {
+      document.getElementById("first").style.display = "none";
+      document.getElementById("second").style.display = "none";
+      document.getElementById("third").style.display = "none";
+      document.getElementById("fourth").style.display = "none";
+      document.getElementById("fifth").style.display = "none";
+      document.getElementById(sectionId).style.display = "inline";
+    }
+
+    function setCurrentDotActive(dotId) {
+      document.getElementById("dot1").style.opacity = "0.3";
+      document.getElementById("dot2").style.opacity = "0.3";
+      document.getElementById("dot3").style.opacity = "0.3";
+      document.getElementById("dot4").style.opacity = "0.3";
+      document.getElementById("dot5").style.opacity = "0.3";
+      document.getElementById(dotId).style.opacity = "1";
+    }
 });
