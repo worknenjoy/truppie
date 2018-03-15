3e8a63996c578860eac9d8497d1fa3b696deaae2
lucianojfj <lucianojfj@gmail.com>
2018-03-15 17:15:53 -0300

    Added Docker to developlment environment #73

--- db/schema.rb
+++ db/schema.rb
@@ -473,7 +473,7 @@ ActiveRecord::Schema.define(version: 20180116235218) do
     t.string   "policy",               default: [],                 array: true
     t.integer  "percent",              default: 3
     t.string   "invite_token"
-    t.boolean  "mail_notification"
+    t.boolean  "mail_notification",    default: true
   end
 
   add_index "organizers", ["marketplace_id"], name: "index_organizers_on_marketplace_id", using: :btree
