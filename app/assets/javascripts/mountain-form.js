49aa302b857c3c2f7bd7c015b98854011e3557f8
lucianojfj <lucianojfj@gmail.com>
2018-03-21 00:54:56 -0300

    Fix #93

--- app/assets/javascripts/mountain-form.js
+++ app/assets/javascripts/mountain-form.js
@@ -1,66 +1,64 @@
-window.addEventListener("load", function () {
-    $(document).on("click", ".container-mountain #continue", nextStep);
-    $(document).on("click", ".container-mountain #continue2", nextStep2);
-    $(document).on("click", ".container-mountain #continue3", nextStep3);
-    $(document).on("click", ".container-mountain #continue4", nextStep4);
-    $(document).on("click", ".container-mountain #prev", prevStep);
-    $(document).on("click", ".container-mountain #prev2", prevStep2);
-    $(document).on("click", ".container-mountain #prev3", prevStep3);
-    $(document).on("click", ".container-mountain #prev4", prevStep4);
+$("#continue").on("click", nextStep);
+$("#continue2").on("click", nextStep2);
+$("#continue3").on("click", nextStep3);
+$("#continue4").on("click", nextStep4);
+$("#prev").on("click", prevStep);
+$("#prev2").on("click", prevStep2);
+$("#prev3").on("click", prevStep3);
+$("#prev4").on("click", prevStep4);
 
-    function nextStep(e) {
-      updateTab("second", "dot2", e);
-    }
+function nextStep(e) {
+  updateTab("second", "dot2", e);
+}
 
-    function prevStep(e) {
-      updateTab("first", "dot1", e);
-    }
+function prevStep(e) {
+  updateTab("first", "dot1", e);
+}
 
-    function nextStep2(e) {
-      updateTab("third", "dot3", e);
-    }
+function nextStep2(e) {
+  updateTab("third", "dot3", e);
+}
 
-    function prevStep2(e) {
-      updateTab("second", "dot2", e);
-    }
+function prevStep2(e) {
+  updateTab("second", "dot2", e);
+}
 
-    function nextStep3(e) {
-      updateTab("fourth", "dot4", e);
-    }
+function nextStep3(e) {
+  updateTab("fourth", "dot4", e);
+}
 
-    function prevStep3(e) {
-      updateTab("third", "dot3", e);
-    }
+function prevStep3(e) {
+  updateTab("third", "dot3", e);
+}
 
-    function nextStep4(e) {
-      updateTab("fifth", "dot5", e);
-    }
+function nextStep4(e) {
+  updateTab("fifth", "dot5", e);
+}
 
-    function prevStep4(e) {
-      updateTab("fourth", "dot4", e);
-    }
+function prevStep4(e) {
+  updateTab("fourth", "dot4", e);
+}
 
-    function updateTab(sectionId, dotId, e) {
-      goToSection(sectionId);
-      setCurrentDotActive(dotId);
-      e.preventDefault();
-    }
+function updateTab(sectionId, dotId, e) {
+  goToSection(sectionId);
+  setCurrentDotActive(dotId);
+  e.preventDefault();
+}
 
-    function goToSection(sectionId) {
-      document.getElementById("first").style.display = "none";
-      document.getElementById("second").style.display = "none";
-      document.getElementById("third").style.display = "none";
-      document.getElementById("fourth").style.display = "none";
-      document.getElementById("fifth").style.display = "none";
-      document.getElementById(sectionId).style.display = "inline";
-    }
+function goToSection(sectionId) {
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("fourth").style.display = "none";
+  document.getElementById("fifth").style.display = "none";
+  document.getElementById(sectionId).style.display = "inline";
+}
 
-    function setCurrentDotActive(dotId) {
-      document.getElementById("dot1").style.opacity = "0.3";
-      document.getElementById("dot2").style.opacity = "0.3";
-      document.getElementById("dot3").style.opacity = "0.3";
-      document.getElementById("dot4").style.opacity = "0.3";
-      document.getElementById("dot5").style.opacity = "0.3";
-      document.getElementById(dotId).style.opacity = "1";
-    }
-});
+function setCurrentDotActive(dotId) {
+  document.getElementById("dot1").style.opacity = "0.3";
+  document.getElementById("dot2").style.opacity = "0.3";
+  document.getElementById("dot3").style.opacity = "0.3";
+  document.getElementById("dot4").style.opacity = "0.3";
+  document.getElementById("dot5").style.opacity = "0.3";
+  document.getElementById(dotId).style.opacity = "1";
+}
